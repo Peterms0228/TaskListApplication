2fd358a4cdb80ce1f2f70d2eb2208e7d2b1ae0bb
peter-030 <chuams-wm19@student.tarc.edu.my>
2023-09-13 13:11:31 +0800

    13/9/2023 13:10 - add due date - add enum priority - add enum status

--- TaskListApplication/mainForm.Designer.cs
+++ TaskListApplication/mainForm.Designer.cs
@@ -34,13 +34,19 @@
             this.tbDesc = new System.Windows.Forms.TextBox();
             this.dgvTaskList = new System.Windows.Forms.DataGridView();
             this.btnEdit = new System.Windows.Forms.Button();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.lblDueDate = new System.Windows.Forms.Label();
+            this.cbPriority = new System.Windows.Forms.ComboBox();
+            this.lblPriority = new System.Windows.Forms.Label();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTaskList)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAdd
             // 
             this.btnAdd.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnAdd.Location = new System.Drawing.Point(795, 68);
+            this.btnAdd.Location = new System.Drawing.Point(1030, 68);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(128, 55);
             this.btnAdd.TabIndex = 0;
@@ -56,7 +62,7 @@
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblTitle.Location = new System.Drawing.Point(12, 9);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(911, 45);
+            this.lblTitle.Size = new System.Drawing.Size(1146, 45);
             this.lblTitle.TabIndex = 1;
             this.lblTitle.Text = "Task List Application";
             // 
@@ -65,7 +71,7 @@
             this.tbTitle.Location = new System.Drawing.Point(12, 68);
             this.tbTitle.Name = "tbTitle";
             this.tbTitle.PlaceholderText = "Task Title";
-            this.tbTitle.Size = new System.Drawing.Size(776, 31);
+            this.tbTitle.Size = new System.Drawing.Size(598, 31);
             this.tbTitle.TabIndex = 2;
             // 
             // tbDesc
@@ -74,7 +80,7 @@
             this.tbDesc.Multiline = true;
             this.tbDesc.Name = "tbDesc";
             this.tbDesc.PlaceholderText = "Description";
-            this.tbDesc.Size = new System.Drawing.Size(776, 80);
+            this.tbDesc.Size = new System.Drawing.Size(740, 80);
             this.tbDesc.TabIndex = 2;
             // 
             // dgvTaskList
@@ -84,13 +90,13 @@
             this.dgvTaskList.Location = new System.Drawing.Point(13, 202);
             this.dgvTaskList.Name = "dgvTaskList";
             this.dgvTaskList.RowHeadersWidth = 62;
-            this.dgvTaskList.Size = new System.Drawing.Size(910, 436);
+            this.dgvTaskList.Size = new System.Drawing.Size(1145, 436);
             this.dgvTaskList.TabIndex = 3;
             this.dgvTaskList.Text = "dataGridView1";
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(795, 129);
+            this.btnEdit.Location = new System.Drawing.Point(1030, 129);
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(128, 55);
             this.btnEdit.TabIndex = 4;
@@ -98,12 +104,72 @@
             this.btnEdit.UseVisualStyleBackColor = true;
             this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.CustomFormat = "dd-MM-yyyy";
+            this.dtpDueDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDueDate.Location = new System.Drawing.Point(862, 68);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(149, 31);
+            this.dtpDueDate.TabIndex = 5;
+            this.dtpDueDate.Value = new System.DateTime(2023, 9, 13, 10, 32, 7, 0);
+            // 
+            // lblDueDate
+            // 
+            this.lblDueDate.AutoSize = true;
+            this.lblDueDate.Location = new System.Drawing.Point(766, 68);
+            this.lblDueDate.Name = "lblDueDate";
+            this.lblDueDate.Size = new System.Drawing.Size(90, 25);
+            this.lblDueDate.TabIndex = 6;
+            this.lblDueDate.Text = "Due Date:";
+            // 
+            // cbPriority
+            // 
+            this.cbPriority.FormattingEnabled = true;
+            this.cbPriority.Location = new System.Drawing.Point(862, 105);
+            this.cbPriority.Name = "cbPriority";
+            this.cbPriority.Size = new System.Drawing.Size(150, 33);
+            this.cbPriority.TabIndex = 7;
+            // 
+            // lblPriority
+            // 
+            this.lblPriority.AutoSize = true;
+            this.lblPriority.Location = new System.Drawing.Point(784, 105);
+            this.lblPriority.Name = "lblPriority";
+            this.lblPriority.Size = new System.Drawing.Size(72, 25);
+            this.lblPriority.TabIndex = 8;
+            this.lblPriority.Text = "Priority:";
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.Enabled = false;
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Location = new System.Drawing.Point(862, 145);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(149, 33);
+            this.cbStatus.TabIndex = 9;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(792, 144);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(64, 25);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "Status:";
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(935, 650);
+            this.ClientSize = new System.Drawing.Size(1170, 650);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cbStatus);
+            this.Controls.Add(this.lblPriority);
+            this.Controls.Add(this.cbPriority);
+            this.Controls.Add(this.lblDueDate);
+            this.Controls.Add(this.dtpDueDate);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.dgvTaskList);
             this.Controls.Add(this.tbDesc);
@@ -127,6 +193,12 @@
         private System.Windows.Forms.TextBox tbDesc;
         private System.Windows.Forms.DataGridView dgvTaskList;
         private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.Label lblDueDate;
+        private System.Windows.Forms.ComboBox cbPriority;
+        private System.Windows.Forms.Label lblPriority;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- TaskListApplication/mainForm.cs
+++ TaskListApplication/mainForm.cs
@@ -13,23 +13,28 @@ namespace TaskListApplication
 {
     public partial class mainForm : Form
     {
+        enum Status { Pending, Completed, Expired };
+        enum Priority { High, Medium, Low };
+
         DataTable taskList = new DataTable();
         Boolean isEditing = false;
         string filePath = "taskList.csv";
+        
         public mainForm()
         {
             InitializeComponent();
-
             try
             {
+                //load data
                 loadDataTableFromCSV(filePath);
             }
             catch (Exception e) 
             {
-                //first time create table
+                //trigger if first time create table
                 taskList.Columns.Add("Task Title");
                 taskList.Columns.Add("Description");
-                taskList.Columns.Add("Date");
+                taskList.Columns.Add("Priority");
+                taskList.Columns.Add("Due Date");
                 taskList.Columns.Add("Status");
             }
         }
@@ -37,6 +42,9 @@ namespace TaskListApplication
         {
             tbTitle.Text = "";
             tbDesc.Text = "";
+            cbPriority.SelectedItem = Priority.Medium;
+            dtpDueDate.Value = DateTime.Now;
+            cbStatus.SelectedItem = Status.Pending;
         }
 
         // Function to save DataTable to a CSV file
@@ -50,10 +58,10 @@ namespace TaskListApplication
                     writer.Write(column.ColumnName);
                     if (column.Ordinal < dataTable.Columns.Count - 1)
                     {
-                        writer.Write(","); // Add a comma if it's not the last column
+                        writer.Write(","); // Add a ',' if it's not the last column
                     }
                 }
-                writer.WriteLine(); // Move to the next line
+                writer.WriteLine(); 
 
                 // Write the data rows
                 foreach (DataRow row in dataTable.Rows)
@@ -63,10 +71,10 @@ namespace TaskListApplication
                         writer.Write(row[i].ToString());
                         if (i < dataTable.Columns.Count - 1)
                         {
-                            writer.Write(","); // Add a comma if it's not the last column
+                            writer.Write(","); // Add a ',' if it's not the last column
                         }
                     }
-                    writer.WriteLine(); // Move to the next line
+                    writer.WriteLine(); 
                 }
             }
         }
@@ -96,39 +104,73 @@ namespace TaskListApplication
 
         private void mainForm_Load(object sender, EventArgs e)
         {
+            //inital priority = medium
+            cbPriority.DataSource = Enum.GetValues(typeof(Priority));
+            cbPriority.SelectedItem = Priority.Medium;
+
+            //initial status = pending
+            cbStatus.DataSource = Enum.GetValues(typeof(Status));
+            cbStatus.SelectedItem = Status.Pending;
+
+            //inital dueDate = today 
+            dtpDueDate.Value = DateTime.Now;
+
+            //set TaskList datasource
             dgvTaskList.DataSource = taskList;
        
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            DateTime currentDateTime = DateTime.Now;
-            taskList.Rows.Add(tbTitle.Text, tbDesc.Text, currentDateTime, "Pending");
+            //add data to data list
+            var title = tbTitle.Text;
+            var desc = tbDesc.Text;
+            var prio = cbPriority.SelectedItem;
+            var dueDate = dtpDueDate.Value.ToString("dd/MM/yyyy");
+            var status = cbStatus.SelectedValue;
+            taskList.Rows.Add(title, desc, prio, dueDate, status);
 
             clearField();
-
             saveDataTableToCSV(taskList, filePath);
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-
             if (!isEditing)
             {
-                //grad data from list to text box
-                tbTitle.Text = taskList.Rows[dgvTaskList.CurrentCell.RowIndex].ItemArray[0].ToString();
-                tbDesc.Text = taskList.Rows[dgvTaskList.CurrentCell.RowIndex].ItemArray[1].ToString();
+                //grab data from list to text box
+                tbTitle.Text = taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Task Title"].ToString();
+                tbDesc.Text = taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Description"].ToString();
 
+                var strPriority = taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Priority"].ToString();
+                cbPriority.SelectedItem = (Priority)Enum.Parse(typeof(Priority), strPriority);
+
+                var strDueDate = taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Due Date"].ToString();
+                dtpDueDate.Value = DateTime.Parse(strDueDate);
+
+                cbStatus.Enabled = true;
+                var strStatus = taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Status"].ToString();
+                cbStatus.SelectedItem = (Status)Enum.Parse(typeof(Status), strStatus);
+
+                //editing
                 isEditing = true;
+                btnEdit.Text = "Editing...";
             }
             else
             {
+                //set data to list
                 taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Task Title"] = tbTitle.Text;
                 taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Description"] = tbDesc.Text;
+                taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Priority"] = cbPriority.Text;
+                taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Due Date"] = dtpDueDate.Value.ToString("dd/MM/yyyy");
+                taskList.Rows[dgvTaskList.CurrentCell.RowIndex]["Status"] = cbStatus.Text;
 
                 //end of editing
+                cbStatus.Enabled = false;
                 isEditing = false;
+                btnEdit.Text = "Edit";
                 clearField();
+                saveDataTableToCSV(taskList, filePath);
             }
         }
     }
